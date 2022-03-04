3683a3d1b526bc6d71bafd68a59dad800b1a31c1
25garcial <91680716+25garcial@users.noreply.github.com>
2022-03-04 12:10:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,86 @@
-function computerPlay(){
-    let pick=Math.floor(Math.random()*3);
-    
-    if (pick===2){
-        return "rock"
-    }
-    else if (pick===1){
-        return "paper";
-    }
-    else { 
-        return "scissors";
-    }
-    
+function computerPlay() {
+  let pick = Math.floor(Math.random() * 3);
+
+  if (pick === 2) {
+    return "rock"
+  } else if (pick === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+
 }
 
 
-function playRound(playerChoice, computerChoice){
-    if (playerChoice===computerChoice){
-      return "Tie."
-      }
-      
-    else if (playerChoice==="paper"){
-    
-      if (computerChoice==="rock"){
-        return "You win.";
-      }
-      else if (computerChoice==="scissors"){
-        return "computer";
-        alert("You lose. Scissors beats paper.");
-      }
+function playRound(playerChoice, computerChoice) {
+
+  console.log(`player choice was ${playerChoice} and computer chose ${computerChoice}`);
+  if (playerChoice === computerChoice) {
+    return "Tie."
+
+  } else if (playerChoice === "paper") {
+
+    if (computerChoice === "rock") {
+      return "You win that round.";
+    } else if (computerChoice === "scissors") {
+      alert("You lose. Scissors beats paper.");
+      return "computer";
     }
-    else if (playerChoice==="rock"){
-      if (computerChoice==="paper"){
-        return computer;
-        alert("You lose. Paper beats rock.")
-      }
-      else if (computerChoice==="scissors"){
-        return "You win.";
-      }
+  } else if (playerChoice === "rock") {
+    if (computerChoice === "paper") {
+      alert("You lose. Paper beats rock.")
+      return "computer";
+    } else if (computerChoice === "scissors") {
+      return "You win that round.";
     }
-    else if (playerChoice==="scissors"){
-      if (computerChoice==="paper"){
-        return "You win.";
-     }
-      else if (computerChoice==="rock"){
-        return "computer";
-        alert("You lose. Rock beats scissors.");
-      }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "paper") {
+      return "You win that round.";
+    } else if (computerChoice === "rock") {
+      alert("You lose. Rock beats scissors.");
+      return "computer";
     }
-    else{
-        return "That is not an option.";
+  } else {
+    return "That is not an option.";
+  }
+}
+
+function playerChoice() {
+  let pick = prompt("Pick rock, paper, or scissors.");
+  let final = pick.toLowerCase();
+  return final;
+}
+
+function game() {
+  let rounds = prompt("How many rounds?");
+  alert(`Best out of ${rounds}.`);
+  let playerScore = 0;
+  let compScore = 0;
+  let game = 0;
+  let i = 0;
+  while (i < rounds) {
+    i++
+    game = playRound(playerChoice(), computerPlay());
+
+    if (game === "Tie." || game === "That is not an option.") {
+      alert(game + `  Score is Player:${playerScore}  Computer:${compScore}`);
+    } else if (game === "You win that round.") {
+      playerScore++;
+      alert(game + ` Score is Player:${playerScore}  Computer:${compScore}`);
+    } else {
+      compScore++;
+      alert(`Computer won that round.  Score is Player:${playerScore}  Computer:${compScore}`);
     }
   }
-function playerChoice(){
-    let pick=prompt("Pick rock, paper, or scissors.");
-    let final=pick.toLowerCase();
-    return final;
-}
-function game(player, computer){
-    let playerScore=0;
-    let compScore=0;
-    let game=0;
-    for (let i=0, i>=5, i++){
-        game=playRound(player, computer);
-        if (game==="Tie" || game==="That is not an option"){
-            alert(game);
-        }
-        else if (game==="You win."){
-            playerScore++;
-            alert(game);
-        }
-        else{
-            compScore++;
-            alert(game);
-        }
-    }
-    if (compScore>playerScore){
-        alert("Computer wins.");
-    }
-    else if (playerScore>compScore){
-        alert("You win.");
-    }
-    else{
+
+  if (compScore > playerScore) {
+    alert("Computer wins the game.");
+  } else if (playerScore > compScore) {
+    alert("You win the game.");
+  } else {
     alert("Tie game.")
-    }
+  }
 }
+let button = document.querySelector("button");
+
+button.onclick = game()
